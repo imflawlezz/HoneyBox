01bc108148fed0305cf35c10ec46027c803ca547
imflawlezz <egorart6@gmail.com>
2026-04-06 21:42:25 +0200

    Initial Commit

---
+++ HoneyBox/HoneyBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneyBoxApp.swift
+//  HoneyBox
+//
+//  Created by Yahor Artsiomchyk on 06/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct HoneyBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
